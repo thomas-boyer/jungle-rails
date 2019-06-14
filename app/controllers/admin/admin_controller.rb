038820cc13481e9a642cf5452821191c13edca3b
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-06-14 18:17:54 +0000

    Display message in cart page if cart is empty

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,5 @@
 class Admin::AdminController < ApplicationController
 
-  http_basic_authenticate_with name: ADMIN_USERNAME, password: ADMIN_PASSWORD
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
 end
